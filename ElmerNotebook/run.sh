bbca28ee559d532c6c017b1b33a36ada8e869549
Eelis Takala <eelis.takala@gmail.com>
2022-08-09 20:59:57 +0300

    Add parallel support to run.sh

--- ElmerNotebook/run.sh
+++ ElmerNotebook/run.sh
@@ -1,5 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
+MPI=true
+
+mkdir -p Log
 echo "Removing old log files:"
 rm -v ./Log/*
 
@@ -20,7 +23,13 @@ LOGFILE="./Log/$logname$DATE.log"
 echo " "
 echo "*** ELMERSOLVER ***"
 echo "*** Cleaning past results" | tee $LOGFILE
-rm -v ./RESU/case* | tee -a $LOGFILE
+rm -v "./RESU/case*"| tee -a $LOGFILE
 echo "*** Run the coil powering: coil-energization.sif" | tee $LOGFILE
-ElmerSolver case.sif | tee -a $LOGFILE
-#mpirun -np 4 coil-energization.sif | tee -a $LOGFILE
+
+if [ $MPI == true ]
+then
+	ElmerGrid 2 2 MESH -metis 4 3 -partdual
+	mpirun -np 4 ElmerSolver_mpi "case.sif" | tee -a $LOGFILE
+else
+	ElmerSolver "case.sif" | tee -a $LOGFILE
+fi
